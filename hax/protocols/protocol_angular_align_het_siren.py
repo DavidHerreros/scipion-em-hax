39ce9d7686efff7df9cb62a13deabcf6bd9696ca
Eduardo Garcia <egarcia@cnb.csic.es>
2025-10-22 14:28:01 +0200

    HetSIREN protocol for Hax Plugin in Scipion

---
+++ hax/protocols/protocol_angular_align_het_siren.py
@@ -0,0 +1,375 @@
+# **************************************************************************
+# *
+# * Authors:     Eduardo García Delgado (eduardo.garcia@cnb.csic.es) [1]
+# *              David Herreros Calero (dherreos@cnb.csic.es) [1]
+# *
+# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC [1]
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+
+import os
+import numpy as np
+from glob import glob
+
+from xmipp_metadata.metadata import XmippMetaData
+from xmipp_metadata.image_handler import ImageHandler
+
+import pyworkflow.protocol.params as params
+from pyworkflow.object import String
+from pyworkflow.utils.path import moveFile
+from pyworkflow import VERSION_1
+from pyworkflow.utils import getExt
+
+from pwem.protocols import ProtAnalysis3D, ProtFlexBase
+from pwem.objects import Volume, ParticleFlex
+
+import xmipp3
+from xmipp3.convert import createItemMatrix, setXmippAttributes, writeSetOfParticles, geometryFromMatrix, matrixFromGeometry
+
+import hax
+import hax.constants as const
+
+class JaxProtAngularAlignmentHetSiren(ProtAnalysis3D, ProtFlexBase):
+    """ Protocol for angular alignment with heterogeneous reconstruction with the HetSIREN algorithm."""
+    _label = 'flexible align - HetSIREN'
+    _lastUpdateVersion = VERSION_1
+
+    # --------------------------- DEFINE param functions -----------------------
+    def _defineParams(self, form):
+        form.addSection(label='Input')
+        form.addHidden(params.USE_GPU, params.BooleanParam, default=True,
+                       label="Use GPU for execution",
+                       help="This protocol has both CPU and GPU implementation.\
+                                 Select the one you want to use.")
+
+        form.addHidden(params.GPU_LIST, params.StringParam, default='0',
+                       expertLevel=params.LEVEL_ADVANCED,
+                       label="Choose GPU IDs",
+                       help="Add a list of GPU devices that can be used")
+
+        group = form.addGroup("Data")
+        group.addParam('inputParticles', params.PointerParam, label="Input particles", pointerClass='SetOfParticles')
+
+        group.addParam('inputVolume', params.PointerParam, allowsNull=True,
+                       label="Starting volume", pointerClass='Volume',
+                       help="If provided, the HomoSIREN network will learn to refine with the new learned angles. "
+                            "Otherwise, the network will learn the reconstruction of the map from scratch")
+
+        group.addParam('inputVolumeMask', params.PointerParam,
+                       label="Reconstruction mask", pointerClass='VolumeMask', allowsNull=True,
+                       help="If provided, the pose refinement and reconstruction learned by HomoSIREN will be focused "
+                            "in the region delimited by the mask. Otherise, a sphere inscribed in the volume box will "
+                            "be used")
+
+        group.addParam('boxSize', params.IntParam, default=128,
+                       label='Downsample particles to this box size',
+                       help='In general, downsampling the particles will increase performance without compromising '
+                            'the estimation the deformation field for each particle. Note that output particles will '
+                            'have the original box size, and Zernike3D coefficients will be modified to work with the '
+                            'original size images')
+
+        group.addParam('ctfType', params.EnumParam, choices=['None', 'Apply', 'Wiener', 'Precorrect'],
+                       default=1, label="CTF correction type",
+                       display=params.EnumParam.DISPLAY_HLIST,
+                       condition="applyCTF", expertLevel=params.LEVEL_ADVANCED,
+                       help="* *Apply*: CTF is applied to the projection generated from the reference map\n"
+                            "* *Wiener*: input particle is CTF corrected by a Wiener fiter\n"
+                            "* *Precorrect: similar to Wiener but CTF has already been corrected")
+
+        form.addSection(label='Network')
+        form.addParam('fineTune', params.EnumParam, default=False,
+                      label='Type of reloading?', display=params.EnumParam.DISPLAY_HLIST,
+                      help='If fineTune, a previously trained deepPose network will be fine tuned based on the '
+                           'new input parameters.')
+
+        form.addParam('netProtocol', params.PointerParam, label="Previously trained network",
+                      allowsNull=True,
+                      pointerClass='JaxProtAngularAlignmentHetSiren',
+                      condition="fineTune==True")
+
+        form.addParam('lazyLoad', params.BooleanParam, default=False,
+                      expertLevel=params.LEVEL_ADVANCED, label='Lazy loading into RAM',
+                      help='If provided, images will be loaded to RAM. This is recommended if you want the best performance '
+                           'and your dataset fits in your RAM memory. If this flag is not provided, '
+                           'images will be memory mapped. When this happens, the program will trade disk space for performance. '
+                           'Thus, during the execution additional disk space will be used and the performance '
+                           'will be slightly lower compared to loading the images to RAM. Disk usage will be back to normal '
+                           'once the execution has finished.')
+
+        group = form.addGroup("Network hyperparameters")
+        group.addParam('latDim', params.IntParam, default=10, label='Latent space dimension',
+                       expertLevel=params.LEVEL_ADVANCED,
+                       help="Dimension of the HetSIREN bottleneck (latent space dimension)")
+
+        group.addParam('epochs', params.IntParam, default=20, condition="stopType==1",
+                       label='Number of training epochs')
+
+        group.addParam('batchSize', params.IntParam, default=8, label='Number of images in batch',
+                       help="Number of images that will be used simultaneously for every training step. "
+                            "We do not recommend to change this value unless you experience memory errors. "
+                            "In this case, value should be decreased.")
+
+        form.addSection(label='Reconstruction')
+        form.addParam('massTransport', params.BooleanParam, condition='not localRecon', default=True,
+                      expertLevel=params.LEVEL_ADVANCED,
+                      label='Mass transportation',
+                      help='When set, HetSIREN will be able to "move" the mass inside the mask '
+                           'instead of just reconstructing the volume. This implies that HetSIREN will estimate the motion '
+                           'to be applied to the points within the provided mask, instead of considering them fixed in space. '
+                           'This approach is useful when working with large box sizes that '
+                           'do not fit in GPU memory, or when a more through analysis of motions is desired. '
+                           'If False, HetSIREN program will perform heterogeneous reconstruction.')
+
+        form.addParam('localRecon', params.BooleanParam, expertLevel=params.LEVEL_ADVANCED, default=False,
+                       condition='not massTransport',
+                       label='Local reconstruction',
+                       help='When set, HetSIREN will turn to local heterogeneous reconstruction/refinement mod, '
+                            'focusing the analysis of heterogeneity to a region of interest enclosed by the provided reference mask.')
+
+        form.addParallelSection(threads=0, mpi=4)
+
+    def _createFilenameTemplates(self):
+        """ Centralize how files are called """
+        myDict = {
+            'imgsFn': self._getExtraPath('input_particles.xmd'),
+            'fnVol': self._getExtraPath('volume.mrc'),
+            'fnVolMask': self._getExtraPath('mask.mrc'),
+        }
+        self._updateFilenamesDict(myDict)
+
+    # --------------------------- INSERT steps functions -----------------------
+    def _insertAllSteps(self):
+        self._createFilenameTemplates()
+        self._insertFunctionStep(self.writeMetaDataStep)
+        self._insertFunctionStep(self.trainingPredictStep)
+        self._insertFunctionStep(self.createOutputStep)
+
+    def writeMetaDataStep(self):
+        imgsFn = self._getFileName('imgsFn')
+        fnVol = self._getFileName('fnVol')
+        fnVolMask = self._getFileName('fnVolMask')
+        md_file = self._getFileName('imgsFn')
+
+        inputParticles = self.inputParticles.get()
+        Xdim = inputParticles.getXDim()
+        newXdim = self.boxSize.get()
+        vol_mask_dim = newXdim
+
+        if self.inputVolume.get():
+            ih = ImageHandler()
+            inputVolume = self.inputVolume.get().getFileName()
+            ih.convert(self._getXmippFileName(inputVolume), fnVol)
+            curr_vol_dim = ImageHandler(self._getXmippFileName(inputVolume)).getDimensions()[-1]
+            if curr_vol_dim != vol_mask_dim:
+                self.runJob("xmipp_image_resize",
+                            "-i %s --fourier %d " % (fnVol, vol_mask_dim), numberOfMpi=1,
+                            env=xmipp3.Plugin.getEnviron())
+            ih.setSamplingRate(fnVol, inputParticles.getSamplingRate())
+
+        if self.inputVolumeMask.get():
+            ih = ImageHandler()
+            inputMask = self.inputVolumeMask.get().getFileName()
+            if inputMask:
+                ih.convert(self._getXmippFileName(inputMask), fnVolMask)
+                curr_mask_dim = ImageHandler(self._getXmippFileName(inputMask)).getDimensions()[-1]
+                if curr_mask_dim != vol_mask_dim:
+                    self.runJob("xmipp_image_resize",
+                                "-i %s --dim %d --interp nearest" % (fnVolMask, vol_mask_dim), numberOfMpi=1,
+                                env=xmipp3.Plugin.getEnviron())
+        else:
+            ImageHandler().createCircularMask(fnVolMask, boxSize=vol_mask_dim, is3D=True)
+
+        writeSetOfParticles(inputParticles, imgsFn)
+
+        # Write extra attributes (if needed)
+        md = XmippMetaData(md_file)
+        if hasattr(inputParticles.getFirstItem(), "_xmipp_subtomo_labels"):
+            labels = np.asarray([int(particle._xmipp_subtomo_labels) for particle in inputParticles.iterItems()])
+            md[:, "subtomo_labels"] = labels
+        md.write(md_file, overwrite=True)
+
+        if newXdim != Xdim:
+            params = "-i %s -o %s --save_metadata_stack %s --fourier %d" % \
+                     (imgsFn,
+                      self._getTmpPath('scaled_particles.stk'),
+                      self._getExtraPath('scaled_particles.xmd'),
+                      newXdim)
+            if self.numberOfMpi.get() > 1:
+                params += " --mpi_job_size %d" % int(inputParticles.getSize() / self.numberOfMpi.get())
+            self.runJob("xmipp_image_resize", params, numberOfMpi=self.numberOfMpi.get(),
+                        env=xmipp3.Plugin.getEnviron())
+            moveFile(self._getExtraPath('scaled_particles.xmd'), imgsFn)
+
+    def trainingPredictStep(self):
+        md_file = self._getFileName('imgsFn')
+        vol_file = self._getFileName('fnVol')
+        mask_file = self._getFileName('fnVolMask')
+        out_path = self._getExtraPath()
+        if not os.path.isdir(out_path):
+            os.mkdir(out_path)
+        batch_size = self.batchSize.get()
+        epochs = self.epochs.get()
+        latDim = self.latDim.get()
+        newXdim = self.boxSize.get()
+        correctionFactor = self.inputParticles.get().getXDim() / newXdim
+        sr = correctionFactor * self.inputParticles.get().getSamplingRate()
+        args = "--md %s --vol %s --mask %s --sr %f --lat_dim %d --epochs %d --batch_size %d --output_path %s " \
+               % (md_file, vol_file, mask_file, sr, latDim, epochs, batch_size, out_path)
+
+        if self.ctf_type != 0:
+            if self.ctf_type.get() == 1:
+                args += '--ctf_type apply '
+            elif self.ctf_type.get() == 2:
+                args += '--ctf_type wiener '
+            elif self.ctf_type.get() == 3:
+                args += '--ctf_type precorrect '
+
+        if self.lazyLoad:
+            args += '--load_images_to_ram '
+
+        if self.massTransport:
+            args += '--transport_mass '
+        elif self.localRecon:
+            args += '--local_reconstruction '
+
+        if self.useGpu.get():
+            gpu = str(self.getGpuList()[0])
+        else:
+            gpu = ''
+
+        program = hax.Plugin.getProgram("hetsiren.py", gpu)
+        self.runJob(program, args + '--mode train', numberOfMpi=1)
+        self.runJob(program, args + f'--mode predict --reload {self._getExtraPath("HetSIREN")}', numberOfMpi=1)
+
+    def createOutputStep(self):
+        inputParticles = self.inputParticles.get()
+        out_path_vols = self._getExtraPath('volumes')
+        model_path = self._getExtraPath('HetSIREN')
+        md_file = self._getFileName('imgsFn')
+        out_path = self._getExtraPath()
+
+        metadata = XmippMetaData(md_file)
+        latent_space = np.asarray([np.fromstring(item, sep=',') for item in metadata[:, 'latent_space']])
+
+        inputSet = self.inputParticles.get()
+        partSet = self._createSetOfParticlesFlex(progName=const.HETSIREN)
+
+        partSet.copyInfo(inputSet)
+        partSet.setHasCTF(inputSet.hasCTF())
+        partSet.setAlignmentProj()
+
+        idx = 0
+        for particle in inputSet.iterItems():
+            outParticle = ParticleFlex(progName=const.HETSIREN)
+            outParticle.copyInfo(particle)
+            outParticle.setZFlex(latent_space[idx])
+            partSet.append(outParticle)
+            idx += 1
+
+        partSet.getFlexInfo().modelPath = String(model_path)
+
+        if self.inputVolume.get():
+            inputVolume = self.inputVolume.get().getFileName()
+            partSet.getFlexInfo().refMap = String(inputVolume)
+
+        if self.inputVolumeMask.get():
+            inputMask = self.inputVolumeMask.get().getFileName()
+            partSet.refMask = String(inputMask)
+
+        if self.ctf_type.get() != 0:
+            if self.ctfType.get() == 1:
+                partSet.getFlexInfo().ctfType = String("apply")
+            elif self.ctfType.get() == 1:
+                partSet.getFlexInfo().ctfType = String("wiener")
+            elif self.ctf_type.get() == 2:
+                partSet.getFlexInfo().ctfType = String("precorrect")
+
+        if self.useGpu.get():
+            gpu = str(self.getGpuList()[0])
+        else:
+            gpu = ''
+
+        latents_file_txt = os.path.join(out_path, 'latents.txt')
+        np.savetxt(latents_file_txt, latent_space)
+
+        args = "--latents_file %s --output_path %s" % (latents_file_txt, out_path_vols)
+        program = hax.Plugin.getProgram("decode_states_from_latents.py", gpu)
+        self.runJob(program, args, numberOfMpi=1)
+
+        outVols = self._createSetOfVolumes()
+        outVols.setSamplingRate(inputParticles.getSamplingRate())
+        for idx in range(latent_space.shape[0]):
+            outVol = Volume()
+            outVol.setSamplingRate(inputParticles.getSamplingRate())
+
+            ImageHandler().scaleSplines(os.path.join(out_path, f"decoded_volume_{idx:04d}.mrc"),
+                                        os.path.join(out_path_vols, f"decoded_volume_{idx:04d}.mrc"),
+                                        finalDimension=inputParticles.getXDim(), overwrite=True)
+
+            ImageHandler().setSamplingRate(os.path.join(out_path_vols, f"decoded_volume_{idx:04d}.mrc"),
+                                           inputParticles.getSamplingRate())
+
+            outVol.setLocation(os.path.join(out_path_vols, f"decoded_volume_{idx:04d}.mrc"))
+            outVols.append(outVol)
+
+        self._defineOutputs(outputParticles=partSet)
+        self._defineTransformRelation(inputParticles, partSet)
+
+        self._defineOutputs(outputVolumes=outVols)
+        self._defineTransformRelation(inputParticles, outVols)
+
+    # --------------------------- INFO functions -----------------------------
+    def _summary(self):
+        summary = []
+        logFile = os.path.abspath(self._getLogsPath()) + "/run.stdout"
+        with open(logFile, "r") as fi:
+            for ln in fi:
+                if ln.startswith("GPU memory has"):
+                    summary.append(ln)
+                    break
+        return summary
+
+    # ----------------------- VALIDATE functions -----------------------
+    def _validate(self):
+        """ Try to find errors on define params. """
+        errors = []
+
+        mask = self.inputVolumeMask.get()
+
+        if mask is not None:
+            data = ImageHandler(mask.getFileName()).getData()
+            if not np.all(np.logical_and(data >= 0, data <= 1)):
+                errors.append("Mask provided is not binary. Please, provide a binary mask")
+
+        return errors
+
+    def _warnings(self):
+        warnings = []
+
+        return warnings
+
+    # --------------------------- UTILS functions -----------------------
+
+    def _getXmippFileName(self, filename):
+        if getExt(filename) == ".mrc":
+            filename += ":mrc"
+        return filename
